9118956ae98c00cfb980cbd90759abd21660e324
Ryan Rix <ry@n.rix.si>
2012-03-16 20:14:01 -0700

    T: Make the HsmTools plugin register itself in HSM

--- lib/hsm_tools/engine.rb
+++ lib/hsm_tools/engine.rb
@@ -3,5 +3,8 @@ module HsmTools
     config.generators do |g|
       g.test_framework :rspec, :view_specs => false
     end
+    initializer 'hsmtools.register_plugin', :after=>"HsmPlugins" do |app|
+       Hsm::Application::Plugins.add "tools"
+    end
   end
 end
